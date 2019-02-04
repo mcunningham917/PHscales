3ece909089bb0b863610748faa37fdad3de20c43
Maxwell Cunningham <cunningham.mt917@gmail.com>
2019-02-04 10:14:11 -0500

    new script to read JSON, fixed defaults accordingly

--- Defaults.m
+++ Defaults.m
@@ -1,13 +1,20 @@
 %% Define root paths
 
-% Set path for PHtools and ouput file, PHanalysis
-
+%% Root
 phRootFilePath      = fullfile('~','Science','ProgHypso');
 
+%% Inputs 
 phDataFilePath      = fullfile(phRootFilePath, 'PHdata');
 phToolsPath         = fullfile(phRootFilePath, 'PHtools','Core');
+
+%% Outputs
 phAnalysisFilePath     = fullfile(phRootFilePath, 'PHscales');
+resultsFilePath = fullfile('~','Science','ProgHypso','PHscales','Analysis','Results')
+
+%% External toolboxes needed
+
 topoToolboxFilePath = fullfile('~','Science','topotoolbox');
+jsonFilePath = fullfile('~','Science','jsonRead')
 
 
 
---
+++ readJson.m
@@ -0,0 +1,7 @@
+
+Defaults;
+addpath(jsonFilePath);
+
+fileName = fullfile(resultsFilePath,'mode_fitting.json')
+
+data =loadjson(fileName)
