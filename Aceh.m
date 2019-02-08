04c31b5dfb7232064fad3914bb0a84a39c12192c
Maxwell Cunningham <cunningham.mt917@gmail.com>
2019-02-08 16:13:22 -0500

    pushed Aceh analysis

--- Aceh.m
+++ Aceh.m
@@ -17,7 +17,8 @@ addpath(topoToolboxFilePath);
 % SupercatchmentNum can be a single value or list, and contains the ID
 % numbers for all supercatchments to be passed to PHRun.
 
-supercatchmentNum = [24,28,29,37];
+supercatchmentNum = [28,29,37];
+%supercatchmentNum = [24];
 
 pixelLength = 30; % in m
 Ac=((1000*1000)/(30*30))*Ac_sqKm; %convert sq km to pixel num
