7876071345e715ebc6c8fc474b3f81ec66458c46
Maxwell Cunningham <cunningham.mt917@gmail.com>
2019-02-11 10:16:23 -0500

    pushed TW analysis

--- Taiwan.m
+++ Taiwan.m
@@ -1,6 +1,6 @@
 %% Define root paths
 
-groupArea = 'Aceh';
+groupArea = 'Taiwan';
 
 Ac_sqKm = 0.5;
 AcSubFolderName = ['Ac0p5km2'];
@@ -17,7 +17,7 @@ addpath(topoToolboxFilePath);
 % SupercatchmentNum can be a single value or list, and contains the ID
 % numbers for all supercatchments to be passed to PHRun.
 
-supercatchmentNum = [24,28,29,37];
+supercatchmentNum = [2,3,4,5,6,7,8,12,13,14,15,16,17,18,20,21,22,23,24,25,27,30,31,32,33,34];
 
 pixelLength = 30; % in m
 Ac=((1000*1000)/(30*30))*Ac_sqKm; %convert sq km to pixel num
